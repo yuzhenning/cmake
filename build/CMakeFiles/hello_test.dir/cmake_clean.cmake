50724cf28daf11aa532a9f643394b372017a531c
Zhenning Yu <syzxyzn@hotmail.com>
2020-06-04 15:52:49 +0800

    Add files via upload

---
+++ build/CMakeFiles/hello_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_test.dir/useHello.cpp.o"
+  "hello_test.pdb"
+  "hello_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
